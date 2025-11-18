be221c72c28cd56937a1613a603b903b80d42219
Barbara Taskinen <barbara.taskinen@gmail.com>
2025-11-18 10:56:48 +0200

    Add routes for getting and updating dodo

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -1,18 +1,50 @@
 const express = require('express');
 const router = express.Router();
 
-const configs = require('../util/config')
+const configs = require('../util/config');
+const Todo = require('../mongo/models/Todo');
 
-let visits = 0
+let visits = 0;
 
 /* GET index data. */
 router.get('/', async (req, res) => {
-  visits++
+  visits++;
 
   res.send({
     ...configs,
-    visits
+    visits,
   });
+});
+
+/* GET todo with specific ID. */
+router.get('/todos/:id', async (req, res) => {
+  try {
+    const todo = await Todo.findById(req.params.id);
+    if (!todo) {
+      return res.status(404).json({ error: 'Todo not found' });
+    }
+    res.json(todo);
+  } catch (error) {
+    res.status(400).json({ error: 'Invalid ID format' });
+  }
+});
+
+/* Update todo by ID */
+router.put('/todos/:id', async (req, res) => {
+  try {
+    const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!updatedTodo) {
+      return res.status(404).json({ error: 'Todo not found' });
+    }
+
+    res.json(updatedTodo);
+  } catch (err) {
+    res.status(400).json({ error: 'Could not update todo' });
+  }
 });
 
 module.exports = router;
